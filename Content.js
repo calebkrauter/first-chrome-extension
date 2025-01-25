41507e3f9764259eb225fe7fc73bab09807044ee
Caleb Krauter <ghostMe66+github@proton.me>
2025-01-25 02:20:37 -0700

    V1
    // TODO make this adjustable.
    // TODO include color of original theme.

--- Content.js
+++ Content.js
@@ -1,76 +1,93 @@
-const Group = Object.freeze({
-    R: 1,
-    G: 4,
-    B: 7,
-    A: 10,
-});
-
+// TODO make this adjustable.
+// TODO include color of original theme.
 const Light = Object.freeze({
-    MAX: 120,
+    MAX: 40,
 });
 
-
+// This site is poorly affected: https://rgbcolorpicker.com/#google_vignette
+// the lowes site is also poorly affected.
 window.onload = getColors();
 
 function getColors() {
-    for (curElement of document.querySelectorAll("*")) {
-        // backgroundColor or color?
+    // Used chatgpt for some of the selector choices.
+    for (curElement of document.querySelectorAll("nav, header, .navbar, .header, path, svg, img, button, input, a, label, html, body, main, footer, section, article, h1, h2, h3, p, div, header")) {
         curColor = window.getComputedStyle(curElement).backgroundColor;
-        // console.log("CUR COLOR" + curColor);
-        curElement.style.backgroundColor = getDarkColor(parseRGB(toString(curColor)));
+
+        curElement.style.color = "white";
+        curElement.style.backgroundColor = getDarkColor(curColor);
+
+        // console.log(getDarkColor(curColor));
+
 
     }
+    // for (curElement of document.querySelectorAll("path, svg, img, button, input, a, label")) {
+    //     curElement.style.color = "gray";
+    //     curElement.style.backgroundColor = ""
+
+    // }
+    // const navbars = document.querySelectorAll('nav, header, .navbar, .header');
+    // navbars.forEach(nav => {
+    //     nav.style.setProperty('background-color', 'rgb(46, 45, 45)', 'important'); // Add a custom background color if needed
+    // });
+
+
 }
 
-function parseRGB(curColor) {
-    const regex = /^rgba?\(((\d+)?(\.+)?\d+[,][ ]?)((\d+)?(\.+)?\d+[,][ ]?)((\d+)?(\.+)?\d+[,][ ]?)?((\d+)?(\.+)?\d+[\)]?)$/;
-    return curColor.match(regex);
-}
 
 function toString(curColor) {
     return String(curColor);
 }
 
-function getDarkColor(rgbPattern) {
-    // console.log(rgbPattern)
-    let resultString = rgbPattern;
-    let r = rgbPattern[Group.R];
-    let g = rgbPattern[Group.G];
-    let b = rgbPattern[Group.B];
-    let a = 1;
-    if (rgbPattern[Group.R] > Light.MAX) {
-        console.log("TOO BRIGHT")
+function getDarkColor(rgbString) {
+
+    slicedValues = rgbString.slice(rgbString.indexOf("(") + 1, rgbString.indexOf(")"));
+    let rgb = [];
+    rgb = slicedValues.split(", ");
+    // console.log(rgb);
+    let r = rgb[0];
+    let g = rgb[1];
+    let b = rgb[2];
+    let resultString;
+    let a = rgb[3];
+    // console.log(r);
+    if (r > Light.MAX) {
+        // console.log("TOO BRIGHT")
         r = Light.MAX;
-    }
-    if (rgbPattern[Group.G] > Light.MAX) {
-        g = Light.MAX;
-    }
-    if (rgbPattern[Group.B] > Light.MAX) {
-        b = Light.MAX;
-    }
-    if (rgbPattern[Group.A]) {
-        resultString = `rgba(${r}, ${g}, ${b}, ${a})`;
+        // console.log(r);
     }
 
-    resultString = `rgb(${r}, ${g}, ${b})`;
+    if (g > Light.MAX) {
+        // console.log("TOO BRIGHT")
+
+        g = Light.MAX;
+    }
+    if (b > Light.MAX) {
+        // console.log("TOO BRIGHT")
+
+        b = Light.MAX;
+    }
+    if (a !== undefined) {
+        resultString = `rgba(${r}, ${g}, ${b}, ${a})`;
+    } else {
+        resultString = `rgb(${r}, ${g}, ${b})`;
+    }
+
+    // console.log(resultString)
     return resultString;
 }
 
-// Content.js
 
-// TODO look at this to find out what works.
-// window.onload = () => {
-//     // Change the primary color using CSS custom properties (if defined)
-//     document.documentElement.style.setProperty('--primary-color', 'red');
+// From ChatGPT
+// Create a <style> element
+const style = document.createElement('style');
 
-//     // Alternatively, change the body text color (for sites that use it)
-//     document.body.style.color = 'red';
+// Set the CSS rules inside the <style> element
+style.innerHTML = `
 
-//     // If you need to target specific elements with 'primary' color styles
-//     let elements = document.querySelectorAll('*');
-//     elements.forEach(el => {
-//         if (getComputedStyle(el).color !== 'rgb(255, 0, 0)') {
-//             el.style.color = 'red';
-//         }
-//     });
-// };
+  div * {
+    background-color: transparent !important;  /* Reset background for all child elements */
+  }
+`;
+sheet.insertRule('select, option { background-color: transparent !important; }', sheet.cssRules.length);
+// Append the <style> element to the head of the document
+document.head.appendChild(style);
